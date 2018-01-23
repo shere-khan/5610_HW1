683fbb1ab84b368a5bbab043992a528ecf2b41b1
Justin Barry <justin.barry.e@gmail.com>
2018-01-23 11:25:26 -0500

    changed untested impl of calculting both distsnces of d_a and d_b, put into one vector D

--- regression.py
+++ regression.py
@@ -15,7 +15,7 @@ def create_data(m, n):
          Vector([9.9, 0.1, 0.8], 0),
          Vector([6.9, -0.1, 0.6], 2),
          Vector([6.8, -0.3, 5.1], 2),
-         Vector([4.1, 0.3, 5.1], 2),
+         Vector([4.1, 0.3, 5.1], 1),
          Vector([1.3, -0.2, 1.8], 1),
          Vector([4.5, 0.4, 2.0], 0),
          Vector([0.5, 0.0, 2.3], 1),
@@ -41,25 +41,7 @@ def square_sum(x, y):
     return tot
 
 
-if __name__ == '__main__':
-    M = create_data(3, 4)
-
-    d_a = Vector([4.1, -0.1, 2.2], None)
-    d_b = Vector([6.1, 0.4, 1.3], None)
-
-    dists_a = list()
-    dists_b = list()
-
-    for m in M:
-        m.dist = dist(d_a, m)
-        dists_a.append(m)
-
-    dists_a.sort(key=lambda x: x.dist)
-    # dists_b.sort()
-
-    ks_a = dists_a[:4]
-    # ks_b = dists_b[:4]
-
+def calc_maj(a):
     maj = [0, 0, 0]
     for i in ks_a:
         if i.cls == 2:
@@ -69,4 +51,22 @@ if __name__ == '__main__':
         elif i.cls == 0:
             maj[2] += 1
 
-    classifier = max(maj)
+    return max(maj)
+
+
+if __name__ == '__main__':
+    M = create_data(3, 4)
+
+    d_a = Vector([4.1, -0.1, 2.2], None)
+    d_b = Vector([6.1, 0.4, 1.3], None)
+    D = [d_a, d_b]
+
+    for d in D:
+        dists = list()
+        for m in M:
+            m.dist = dist(d, m)
+            dists.append(m)
+
+        dists.sort(key=lambda x: x.dist)
+        ks_a = dists[:3]
+        class_a = calc_maj(ks_a)
